4ebbbd7e316d4f01b85d5a797c8686862182f7fe
mahmedmo <muhammada8@outlook.com>
2024-02-09 13:06:11 -0700

    adjusted files

--- ENSF338-Lab1/P1.py
+++
@@ -1,6 +0,0 @@
-
-def hello_world(number):
-    print("Hi everyone! My name is", name)
-    
-hello_world("<Muhammad, Ahmed>")
-
--- ENSF338-Lab1/tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-def hello_world(number):
-print("Hi everyone! My name is", name)
-hello_world("<First name, Last name>")
