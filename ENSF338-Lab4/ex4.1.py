fe659ccf13b4590d020b2d5c53f2fc0f7893b46b
wzhengy <wilson.zheng@ucalgary.ca>
2024-02-29 22:04:43 -0700

    fixed ex4 q1,q2

--- ENSF338-Lab4/ex4.1.py
+++ ENSF338-Lab4/ex4.1.py
@@ -5,18 +5,18 @@ def processdata(li):
             for j in range(len(li)):
                 li[i] *= 2
 
-#Question 1
-#For Best Case, if li[i] is less or equal to 5 then the inner for loop would not execute.
-#This results in a linear time complexity of O(n)
-#For Worst Case, li[i] fulfills the if condition of li[i] > 5 so that the inner for loop executes.
-#This results in a time complexity of O(n^2) because within the loop
-#For Average Case, the time complexity would be O(n^2) since on average the inner for loop would be executed.
+# Question 1
+# For the Best Case, all elements in li are less or equal to 5 then the inner for loop would not execute, 
+# which results in a linear time complexity of O(n).
+# For the Worst Case, all elements of li fulfill the if condition of li[i] > 5 so the inner for loop executes each time the outer for loop executes, 
+# which results in a time complexity of O(n^2).
+# For the Average Case, the time complexity would be half of the Worst Case, which would be O((n^2)/2), which is the same as O(n^2).
                 
-#Question 2
-#The time complexity for average, best, and worst are not the same.
-#Here is the modified version of the code:
+# Question 2
+# The time complexity for average, best, and worst are not the same.
+# Here is the modified version of the code:
 def processdata_modified(li):
     for i in range(len(li)):
         if li[i] > 5:
-            li[i] *= 2
-#This code will now have complexity of O(n) for best, worse and average.
+            li[i] *= 2**len(li)
+# This code will now have complexity of O(n) for best, worse and average.
